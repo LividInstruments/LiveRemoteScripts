cc5ca6b76ac43572cead29d3e785baa9b0ea3103
pnyboer <pete@lividinstruments.com>
2014-01-31 16:33:29 -0800

    Fixed some controls initialization, added slider color control

--- Livid_Base_M4L/LividBaseM4L.py
+++ Livid_Base_M4L/LividBaseM4L.py
@@ -29,73 +29,81 @@ from _Framework.TransportComponent import TransportComponent # Class encapsulati
 
 
 """ Here we define some global variables """
-CHANNEL = 0		#main channel (0 - 15)
-BASE_PADS = [60, 61, 62, 63, 64, 65, 66, 67, 52, 53, 54, 55, 56, 57, 58, 59, 44, 45, 46, 47, 48, 49, 50, 51, 36, 37, 38, 39, 40, 41, 42, 43]	#there are 32 of these
-BASE_TOUCHSTRIPS = [1, 2, 3, 4, 5, 6, 7, 8, 9]		#there are 9 of these
+CHANNEL = 0   #main channel (0 - 15)
+BASE_PADS = [60, 61, 62, 63, 64, 65, 66, 67, 52, 53, 54, 55, 56, 57, 58, 59, 44, 45, 46, 47, 48, 49, 50, 51, 36, 37, 38, 39, 40, 41, 42, 43]  #there are 32 of these
+BASE_TOUCHSTRIPS = [1, 2, 3, 4, 5, 6, 7, 8, 9]    #there are 9 of these
 BASE_TOUCHPADS = [10, 11, 12, 13, 14, 15, 16, 17] #there are 8 of these
-BASE_BUTTONS = [18, 19, 20, 21, 22, 23, 24, 25]		#there are 8 of these
+BASE_BUTTONS = [18, 19, 20, 21, 22, 23, 24, 25]   #there are 8 of these
 BASE_RUNNERS = [68, 69, 70, 71, 72, 73, 74, 75]   #there are 8 of these top LEDS
 BASE_SIDES = [26,27,28,29,30,31,32,33]   #there are 8 of these side LEDS
 BASE_LCDS = [34, 35]
 
 class LividBaseM4L(ControlSurface):
-	__module__ = __name__
-	__doc__ = " LividBaseM4L controller script "
+  __module__ = __name__
+  __doc__ = " LividBaseM4L controller script "
 
-	def __init__(self, c_instance):
-		super(LividBaseM4L, self).__init__(c_instance)
-		with self.component_guard():
-			self._host_name = 'LividBaseM4L'
-			self._color_type = 'Base'
-			self.log_message("--------------= LividBaseM4L log BEGIN =--------------") 
-			self._setup_controls()
-			
-	"""script initialization methods"""
-	
-	def _setup_controls(self):
-		is_momentary = True
-		self._tfader = [None for index in range(9)]
-		self._tfader_touch = [None for index in range(9)]
-		self._pad = [None for index in range(32)]
-		self._pad_cc = [None for index in range(32)]
-		self._button = [None for index in range(8)]
-		self._tbutton = [None for index in range(8)]
-		self._runnerled = [None for index in range(8)]
-		self._sideled = [None for index in range(8)]
-		self._lcd = [None for index in range(2)]
-		for index in range(9):
-			self._tfader[index] = EncoderElement(MIDI_CC_TYPE, CHANNEL, BASE_TOUCHSTRIPS[index], Live.MidiMap.MapMode.absolute)
-			self._tfader[index].name = 'Fader_' + str(index)
-		for index in range(9):
-			self._tfader_touch[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_TOUCHSTRIPS[index])
-			self._tfader_touch[index].name = 'FaderTouch_' + str(index)
-		for index in range(32):
-			self._pad[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_PADS[index])
-			self._pad[index].name = 'Pad_' + str(index)
-		for index in range(32):
-			self._pad_cc[index] = EncoderElement(MIDI_CC_TYPE, CHANNEL, BASE_PADS[index], Live.MidiMap.MapMode.absolute)
-			self._pad_cc[index].name = 'PadCC_' + str(index)
-		for index in range(8):
-			self._button[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_BUTTONS[index])
-			self._button[index].name = 'Button_' + str(index)
-		for index in range(8):
-			self._tbutton[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_TOUCHPADS[index])
-			self._tbutton[index].name = 'Tbutton_' + str(index)		
-		for index in range(8):
-			self._runnerled[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_RUNNERS[index])
-			self._runnerled[index].name = 'RunnerLED_' + str(index)
-		for index in range(8):
-			self._sideled[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_SIDES[index])
-			self._sideled[index].name = 'SideLED_' + str(index)
-		for index in range(2):
-			self._lcd[index] = ButtonElement(is_momentary, MIDI_CC_TYPE, CHANNEL, BASE_LCDS[index])
-			self._lcd[index].name = 'LCD_' + str(index)
+  def __init__(self, c_instance):
+    super(LividBaseM4L, self).__init__(c_instance)
+    with self.component_guard():
+      self._host_name = 'LividBaseM4L'
+      self._color_type = 'Base'
+      self.log_message("--------------= LividBaseM4L log BEGIN SCRIPT =--------------")
+      self._setup_controls()
+      
+  """script initialization methods"""
+  
+  def _setup_controls(self):
+    is_momentary = True
+    self._tfader = [None for index in range(9)]
+    self._tfader_touch = [None for index in range(9)]
+    self._pad = [None for index in range(32)]
+    self._pad_cc = [None for index in range(32)]
+    self._button = [None for index in range(8)]
+    self._tbutton = [None for index in range(8)]
+    self._runnerled = [None for index in range(8)]
+    self._sideled = [None for index in range(8)]
+    self._lcd = [None for index in range(2)]
+    for index in range(9):
+      self._tfader[index] = EncoderElement(MIDI_CC_TYPE, CHANNEL, BASE_TOUCHSTRIPS[index], Live.MidiMap.MapMode.absolute)
+      self._tfader[index].name = 'Fader_' + str(index)
+    for index in range(9):
+      self._tfader_touch[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_TOUCHSTRIPS[index])
+      self._tfader_touch[index].name = 'FaderTouch_' + str(index)
+    for index in range(32):
+      self._pad[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_PADS[index])
+      self._pad[index].name = 'Pad_' + str(index)
+    for index in range(32):
+      self._pad_cc[index] = EncoderElement(MIDI_CC_TYPE, CHANNEL, BASE_PADS[index], Live.MidiMap.MapMode.absolute)
+      self._pad_cc[index].name = 'PadCC_' + str(index)
+    for index in range(8):
+      self._button[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_BUTTONS[index])
+      self._button[index].name = 'Button_' + str(index)
+    for index in range(8):
+      self._tbutton[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_TOUCHPADS[index])
+      self._tbutton[index].name = 'Tbutton_' + str(index)   
+    for index in range(8):
+      self._runnerled[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_RUNNERS[index])
+      self._runnerled[index].name = 'RunnerLED_' + str(index)
+    for index in range(8):
+      self._sideled[index] = ButtonElement(is_momentary, MIDI_NOTE_TYPE, CHANNEL, BASE_SIDES[index])
+      self._sideled[index].name = 'SideLED_' + str(index)
+    for index in range(2):
+      self._lcd[index] = ButtonElement(is_momentary, MIDI_CC_TYPE, CHANNEL, BASE_LCDS[index])
+      self._lcd[index].name = 'LCD_' + str(index)
     
-	def receive_value(self, value):
-		self._value = value
-        
-	"""LividBaseM4L script disconnection"""
-	def disconnect(self):
-		self.log_message("--------------= LividBaseM4L log END =--------------")
-		ControlSurface.disconnect(self)
-		return None
+  def receive_value(self, value):
+    self._value = value
+
+  def slider_color(self, value):
+    for index in range(9):
+      self._send_midi((191,index+10,value))
+      
+  def aslider_color(self, sli, value):
+    self._send_midi((191,sli+10,value))
+
+    
+  """LividBaseM4L script disconnection"""
+  def disconnect(self):
+    self.log_message("--------------= LividBaseM4L log END =--------------")
+    ControlSurface.disconnect(self)
+    return None
